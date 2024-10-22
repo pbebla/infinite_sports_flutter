b727fed1800d9e5729bb5a496ed2333f6ba910bb
pbebla <pauldinb@gmail.com>
2024-10-21 17:20:56 -0700

    removed windows, web, macos, and linux platforms

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -21,44 +21,17 @@ import 'package:flutter_config/flutter_config.dart';
 /// ```
 class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {
-    if (kIsWeb) {
-      return web;
-    }
     switch (defaultTargetPlatform) {
       case TargetPlatform.android:
         return android;
       case TargetPlatform.iOS:
         return ios;
-      case TargetPlatform.macOS:
-        return macos;
-      case TargetPlatform.windows:
-        throw UnsupportedError(
-          'DefaultFirebaseOptions have not been configured for windows - '
-          'you can reconfigure this by running the FlutterFire CLI again.',
-        );
-      case TargetPlatform.linux:
-        throw UnsupportedError(
-          'DefaultFirebaseOptions have not been configured for linux - '
-          'you can reconfigure this by running the FlutterFire CLI again.',
-        );
       default:
         throw UnsupportedError(
           'DefaultFirebaseOptions are not supported for this platform.',
         );
     }
   }
-
-  static const FirebaseOptions web = FirebaseOptions(
-    apiKey: 'AIzaSyB7wZb2tO1-Fs6GbDADUSTs2Qs3w08Hovw',
-    appId: '1:406099696497:web:87e25e51afe982cd3574d0',
-    messagingSenderId: '406099696497',
-    projectId: 'infinite-sports-app',
-    authDomain: 'flutterfire-e2e-tests.firebaseapp.com',
-    databaseURL:
-        'https://flutterfire-e2e-tests-default-rtdb.europe-west1.firebasedatabase.app',
-    storageBucket: 'infinite-sports-app.appspot.com',
-    measurementId: 'G-JN95N1JV2E',
-  );
 
   static FirebaseOptions android = FirebaseOptions(
     apiKey: FlutterConfig.get('ANDROID_API_KEY') ?? "",
@@ -82,20 +55,5 @@ class DefaultFirebaseOptions {
     iosClientId:
         '248087010229-g45omq0n7ev5jtmbc1ei7a0nuaq02544.apps.googleusercontent.com',
     iosBundleId: 'com.infinitesports.Infinite-Sports',
-  );
-
-  static const FirebaseOptions macos = FirebaseOptions(
-    apiKey: 'AIzaSyDooSUGSf63Ghq02_iIhtnmwMDs4HlWS6c',
-    appId: '1:406099696497:ios:1b423b89c63b82053574d0',
-    messagingSenderId: '406099696497',
-    projectId: 'infinite-sports-app',
-    databaseURL:
-        'https://flutterfire-e2e-tests-default-rtdb.europe-west1.firebasedatabase.app',
-    storageBucket: 'infinite-sports-app.appspot.com',
-    androidClientId:
-        '406099696497-17qn06u8a0dc717u8ul7s49ampk13lul.apps.googleusercontent.com',
-    iosClientId:
-        '406099696497-irb7edfevfkhi6t5s9kbuq1mt1og95rg.apps.googleusercontent.com',
-    iosBundleId: 'io.flutter.plugins.firebase.messaging',
   );
 }
