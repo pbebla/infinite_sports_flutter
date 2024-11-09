3242adaa74dddea7547d19c4a85494c37b59206f
pbebla <pauldinb@gmail.com>
2024-11-08 20:05:28 -0800

    changed title of MaterialApp

--- lib/main.dart
+++ lib/main.dart
@@ -56,7 +56,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Infinite Sports',
       theme: Provider.of<ThemeProvider>(context).themeData,
       home: const MyHomePage(),
     );
